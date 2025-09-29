b61ee994303ba13b9d650d42ad89884999e86b0b
Josip Hanak <josip.hanak@visagetechnologies.com>
2025-09-29 14:37:15 +0200

    Added gt_path and noisy measurement track projection

--- experiments/mon-estimation-experiment/mon.py
+++ experiments/mon-estimation-experiment/mon.py
@@ -1,14 +1,20 @@
 import numpy as np
 from experiments.models.cartesian_models import Models as CartesianModels
+from experiments.models.cartesian_models import generate_cartesian_prior_from_mes
 from experiments.models.polar_models import PolarModels 
 from experiments import utils #top level experiments utils
-from .utils import add #mon-estimation-experiment utils
+from .utils import generate_perfect_trajectory_positions #mon-estimation-experiment utils
 from stonesoup.types.state import GaussianState
+from stonesoup.types.detection import Detection
+from stonesoup.types.groundtruth import GroundTruthState, GroundTruthPath
 from datetime import timedelta
 import plotly.graph_objects as go
 import plotly.io as pio
 pio.renderers.default = "browser"
 from numpy.linalg import pinv
+
+
+
 def plot_positions_numpy(positions, color="blue"):
     fig = go.Figure()
     fig.add_trace(go.Scatter(
@@ -26,44 +32,9 @@ def plot_positions_numpy(positions, color="blue"):
     )
     fig.show()
 
-def generate_perfect_trajectory_positions(model_entry, n_steps: int):
-    
-    dt = 0.5
-    trans_model = model_entry.value["trans_mod"]
-    prior_state = model_entry.value["prior"]
-    
-    current_state = prior_state
-    states = [current_state]  # include initial prior
-    
-    
-    positions = np.zeros((n_steps, 2))
-    
-    positions[0, 0] = current_state.state_vector[0, 0]  # x
-    positions[0, 1] = current_state.state_vector[2, 0]  # y
-    
-    for i in range(1, n_steps):
-        
-        next_state_vector = trans_model.function(
-            current_state,
-            noise=False,
-            time_interval=timedelta(seconds=dt)
-        )
-        
-        
-        next_state = GaussianState(
-        state_vector=next_state_vector,  # use the StateVector object directly
-        covar=current_state.covar.copy(),
-        timestamp=current_state.timestamp + timedelta(seconds=dt)
-        )
 
-        states.append(next_state)
-        current_state = next_state
-        
-        
-        positions[i, 0] = next_state.state_vector[0, 0]  # x
-        positions[i, 1] = next_state.state_vector[2, 0]  # y
-    
-    return states, positions
+
+
 
 def extract_transition_data(states, transition_model, dt):
     #Extracts state vectors (U) and their transitioned state vectors (F)
@@ -113,8 +84,11 @@ def measure_of_nonlinearity(states, transition_model, dt):
 
 
 if __name__=="__main__":
-    states , positions = generate_perfect_trajectory_positions(CartesianModels.COORDINATED_TURN, 50)
+    z0 = np.array([[0.0],   # x0 is row 0 
+               [0.0]]) # y0 is row 1
+    gt_path, detections, positions = generate_perfect_trajectory_positions(CartesianModels.COORDINATED_TURN, z0, 50)
     plot_positions_numpy(positions)
+    print(detections)
     #M= measure_of_nonlinearity(states, CartesianModels.COORDINATED_TURN.value["trans_mod"], 0.5)
     #print(M)
 
--- experiments/mon-estimation-experiment/utils.py
+++ experiments/mon-estimation-experiment/utils.py
@@ -1,2 +1,55 @@
-def add(x,y):
-    return x+y
+from stonesoup.types.state import GaussianState
+from stonesoup.types.detection import Detection
+from stonesoup.types.groundtruth import GroundTruthState, GroundTruthPath
+import numpy as np
+from experiments.models.cartesian_models import Models as CartesianModels
+from experiments.models.cartesian_models import generate_cartesian_prior_from_mes
+from experiments.models.polar_models import PolarModels 
+from experiments import utils #top level experiments utils
+from datetime import timedelta
+
+
+def generate_perfect_trajectory_positions(model_entry, first_point: np.ndarray , n_steps: int):
+    
+    dt = 0.5
+    trans_model, R = model_entry.value["trans_mod"], model_entry.value["meas_mod"].noise_covar    
+    prior_state = generate_cartesian_prior_from_mes(first_point)
+    
+    current_state = prior_state
+
+    positions = np.zeros((n_steps, 2))
+    positions[0, 0], positions[0, 1]  = current_state.state_vector[0, 0], current_state.state_vector[2, 0]  # first x and y
+    
+
+    gt_states = [GroundTruthState(current_state.state_vector, current_state.timestamp)]
+    detections = [Detection(state_vector=utils.make_noisy(positions[0, :].reshape(-1, 1), R),
+                                timestamp=current_state.timestamp)]
+    
+   
+    #TODO: take this and set it up for self.gt_path and self.mes inside the mon-estimation
+    for i in range(1, n_steps):
+        
+        next_state_vector = trans_model.function(
+            current_state,
+            noise=False,
+            time_interval=timedelta(seconds=dt)
+        )
+        
+        
+        current_state = GaussianState(
+        state_vector=next_state_vector,  # use the StateVector object directly
+        covar=current_state.covar.copy(),
+        timestamp=current_state.timestamp + timedelta(seconds=dt)
+        )
+
+        positions[i, 0], positions[i, 1] = current_state.state_vector[0, 0], current_state.state_vector[2, 0]  # x and y
+   
+        
+        gt_states.append(GroundTruthState(current_state.state_vector, current_state.timestamp))
+        detections.append(Detection(state_vector=utils.make_noisy(positions[i, :].reshape(-1, 1), R),
+                                timestamp=current_state.timestamp))
+
+
+    gt_path = GroundTruthPath(gt_states)
+    return gt_path, detections, positions
+
