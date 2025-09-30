63f797d84a46406b29f93c438282e88084f5701e
Josip Hanak <josip.hanak@visagetechnologies.com>
2025-09-30 09:44:43 +0200

    MonData population finished

--- experiments/mon-estimation-experiment/mon-estimation.py
+++ experiments/mon-estimation-experiment/mon-estimation.py
@@ -10,17 +10,47 @@ from .utils import generate_perfect_trajectory_positions, generate_perfect_traje
 
 
 class MonData:
-    def __init__(self, data_set_size, path_length):
+    def __init__(self, initial_dataset, path_length):
         """Initializes the dataset, form a path from each starting data point from the dataset, calculates MoN"""
-        dataset = [[0.0, 0.0]]
-        path_length
-        self.paths = [PathData(data_point, path_length) for data_point in dataset]
-        self.mons = [Mon() for _ in range(path_length-1)] #NOTE: MON is calculated on one timestamp of the whole dataset
+        self.paths = [PathData(data_point, path_length) for data_point in initial_dataset]
+        self.make_gt_and_estimated_dataset()
+        #self.mons = [Mon() for _ in range(path_length-1)] #NOTE: MON is calculated on one timestamp of the whole dataset
         return
     
-    def step():
-        """Should collect gt_point from all paths for a specific step in the trajectory"""
-        return
+    def make_gt_and_estimated_dataset(self):
+        # shape: (n_paths, n_steps, state_dim)
+        self.gt_dataset = np.stack([path.GT_PATH for path in self.paths], axis=0)  
+        self.estimated_dataset = np.stack([path.EST for path in self.paths], axis=0)
+   
+    #if
+    # n_paths = 100
+    # path_length = 40
+    # state_dim = 5
+    #then
+    # self.gt_dataset.shape == (100, 40, 5)
+    # self.get_2d_step_gt_dataset(10).shape == (100, 5) -> all paths, step 10
+    # self.get_2d_step_est_dataset(10).shape == (100, 5) -> all paths, step 10
+
+
+    def get_2d_step_gt_dataset(self,i):
+        """Return ground-truth state of all paths at step i"""
+        if i==0:
+            return self.initial_dataset
+        return self.gt_dataset[:, i, :] 
+
+    def get_2d_step_est_dataset(self,i):
+        """Return estimated state of all paths at step i"""
+        if i==0:
+            print("No point in calculating for initial dataset as the transition function has not been applied")
+        return self.estimated_dataset[:, i, :] # shape: (n_paths, state_dim)
+
+    def get_data_step(self, i):
+        """Retrieves ground truth data set for all points for path step and estimated dataset for path step """
+        if i==0:
+            print("First position the same for estimated and groundtruth")
+        for path in self.paths:
+            self.gt_dataset.append(path.GT_PATH[i])
+            self.estimated_dataset.append(path.EST[i])
 
  
 class PathData:
@@ -88,13 +118,19 @@ class Mon():
 
 if __name__=="__main__":
     # movements = None #TODO: FILL WITH STELA'S DATASET
-    # type = "COORDINATED_TURN"
-    timer_start = datetime.now()
+    type = "COORDINATED_TURN"
+    #timer_start = datetime.now()
     z0 = np.array([[0.0],   # x0 is row 0 
-               [0.0]]) # y0 is row 1
-    path = PathData(z0, 40)
-    timer_end = datetime.now() 
-    print(path.GT_PATH[:20])
-    elapsed = timer_end - timer_start
-    print(f"Time to initialize 1 path: {elapsed.total_seconds():.6f} seconds")
+                [0.0]]) # y0 is row 1
+    path = PathData(z0, 30)
+    # timer_end = datetime.now() 
+    print(path.EST[4])
+    # elapsed = timer_end - timer_start
+    # print(f"Time to initialize 1 path: {elapsed.total_seconds():.6f} seconds")
     #plot_states_xy(path.GT_PATH)
+    timer_start = datetime.now()
+    initial_dataset = [np.array([[0.0], [0.0]])]
+    mon = MonData(initial_dataset, 30)
+    timer_end = datetime.now() 
+    elapsed = timer_end - timer_start
+    print(mon.get_2d_step_est_dataset(4))
